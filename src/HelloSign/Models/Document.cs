b6ce33457e47dd870474ece29b49b0fc5ec658a4
thatquan <86083738+thatquan@users.noreply.github.com>
2022-04-12 23:30:27 -0700

    add FieldGroups to Document model
    enable template callbacks to have more valuable information

--- src/HelloSign/Models/Document.cs
+++ src/HelloSign/Models/Document.cs
@@ -5,12 +5,14 @@ namespace HelloSign
     public class Document
     {
         public List<CustomField> CustomFields { get; set; }
+        public List<FieldGroup> FieldGroups { get; set; }
         public int Index { get; set; }
         public string Name { get; set; }
 
         public Document()
         {
             CustomFields = new List<CustomField>();
+            FieldGroups = new List<FieldGroup>();
         }
     }
 }
